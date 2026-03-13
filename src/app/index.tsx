584b3fa4fc8052389b8a88fb222f433b6b569f87
SARTHAK ATRISH <sarthakatrish@SARTHAKs-MacBook-Air-2.local>
2026-03-13 22:06:50 +0530

    feat: Search Bar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, Button } from 'react-native';
 import FoodListItem from '../components/FoodListItem';
+import { useState } from 'react';
 
 const foodItems = [
   { label: "Pizza", cal: 60, brand: "Dominos" },
@@ -8,8 +9,27 @@ const foodItems = [
 ]
 
 export default function App() {
+
+  const [search, setSearch] = useState('');
+
+  const performSearch = () => {
+    console.warn('Searching for: ', search);
+
+    setSearch('');
+  }
+
+
   return (
     <View style={styles.container}>
+      <TextInput
+        value={search}
+        onChangeText={setSearch}
+        placeholder='Search'
+        style={styles.input}
+      />
+
+      {search && <Button title="Search" onPress={performSearch} />}
+
       <FlatList
         data={foodItems}
         renderItem={({ item }) => <FoodListItem item={item} />}
@@ -23,8 +43,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
     padding: 10,
-    gap: 5,
+    gap: 10
   },
+
+  input: {
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+  }
 });
