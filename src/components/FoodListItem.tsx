e7ff8f78180d1de577946976cb762b05361890c1
SARTHAK ATRISH <sarthakatrish@SARTHAKs-MacBook-Air-2.local>
2026-03-13 21:39:48 +0530

    feat: FlatList style fooditems

--- src/components/FoodListItem.tsx
+++ src/components/FoodListItem.tsx
@@ -1,25 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import Feather from '@expo/vector-icons/Feather';
 
 const FoodListItem = ({ item }) => {
     return (
-        <View
-            style={{
-                backgroundColor: 'gainsboro',
-                padding: 10,
-                borderRadius: 5,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-            }}
-        >
+        <View style={styles.container}>
             <View style={{ flex: 1, gap: 5 }}>
                 <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{item.label}</Text>
                 <Text style={{ fontSize: 14, color: 'dimgray' }}>{item.cal} cal, {item.brand}</Text>
             </View>
-            <Feather name="plus-circle" size={24} color="blue" />
+            <Feather name="plus-circle" size={24} color="royalblue" />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'gainsboro',
+        padding: 10,
+        borderRadius: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    }
+})
+
 
 export default FoodListItem;
